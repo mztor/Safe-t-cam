a020c3d92f866250fd2548d3d4d74745df4719d5
Your <michaelzenkis@hotmail.com>
2017-06-29 18:26:06 +1000

    Added some extra comments

--- speeding.py
+++ speeding.py
@@ -9,7 +9,7 @@ from datetime import datetime #importing date and time module
 DIST_1 = 133 # Distance between Camera 1 and Camera 2
 DIST_2 = 57.5 # Distance between Camera 2 and Camera 3
 SPEED_LIMIT = 110 # Speed limit between all cameras
-TIME_FMT = "%H:%M:%S" # Format of times
+TIME_FMT = "%H:%M:%S" # Format of times involved within the Script
 
 
 def main():
@@ -41,7 +41,7 @@ def main():
             diffobj = time2obj - time1obj # Minus the later time from the earlier time to get difference or time taken to travel between them
             diff_hours = float(diffobj.seconds)/float(3600)
             avg_speed = DIST_1/diff_hours # calculates average speed of number plate between checkpoints 1 & 2
-            avg_speed = round(avg_speed, 1)
+            avg_speed = round(avg_speed, 1) # Rounds the result of avg_speed to 1DP
             if avg_speed > SPEED_LIMIT:
                 speeding_str = highway_times['2'] + " 2 " + plate + " " + str(avg_speed) # Create a string with appropriate information if car is speeding
                 speeding.append(speeding_str) # add the above string to a list of speeding cars
@@ -53,8 +53,8 @@ def main():
             avg_speed = DIST_2/diff_hours
             avg_speed = round(avg_speed, 1)
             if avg_speed > SPEED_LIMIT:
-                speeding_str = highway_times['3'] + " 3 " + plate + " " + str(avg_speed)
-                speeding.append(speeding_str)
+                speeding_str = highway_times['3'] + " 3 " + plate + " " + str(avg_speed) # Creates the required output string
+                speeding.append(speeding_str) # Adds the created string to the speeding list
 
     for item in speeding:
         print(item)
